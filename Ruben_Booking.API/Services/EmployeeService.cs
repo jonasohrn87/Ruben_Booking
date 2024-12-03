fb32fa8bd1ef869045758ea67c64f834aeeff6bd
Olle Ristenstrand <olelwow@hotmail.com>
2024-12-03 15:05:24 +0100

    Created test for endpoint with moq. Created necesseary classes in API project.

---
+++ Ruben_Booking.API/Services/EmployeeService.cs
@@ -0,0 +1,25 @@
+﻿using Ruben_Booking.API.Database;
+using Ruben_Booking.API.Database.Models;
+using Ruben_Booking.API.Services.Interfaces;
+
+namespace Ruben_Booking.API.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly RubenContext _context;
+        public EmployeeService(RubenContext context)
+        {
+            _context = context;
+        }
+        public async Task<IResult> GetEmployeeById(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+
+            if (employee == null) 
+            {
+                return Results.NotFound($"No employee with id {id} was found.");
+            }
+            return Results.Ok(employee);
+        }
+    }
+}
---
+++ Ruben_Booking.API/Services/Interfaces/IEmployeeService.cs
@@ -0,0 +1,9 @@
+﻿using Ruben_Booking.API.Database.Models;
+
+namespace Ruben_Booking.API.Services.Interfaces
+{
+    public interface IEmployeeService
+    {
+        Task<IResult> GetEmployeeById(int id);
+    }
+}
