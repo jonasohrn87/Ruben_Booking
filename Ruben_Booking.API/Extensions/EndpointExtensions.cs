725a527983468f191237d5ba707b03fafdd5d4ef
Olle Ristenstrand <olelwow@hotmail.com>
2024-12-06 10:01:16 +0100

    Refactored the Main method in program. Created extensions folder and respective classes to better organize the code and increase maintainability.

---
+++ Ruben_Booking.API/Extensions/EndpointExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Ruben_Booking.API.Endpoints;
+
+namespace Ruben_Booking.API.Extensions
+{
+    public static class EndpointExtensions
+    {
+        public static void MapApplicationEndpoints(this WebApplication app)
+        {
+            app.MapEmployeeEndpoints();
+            app.MapConsultantEndpoints();
+            app.MapLoginEndpoints();
+            app.MapRoomEndpoints();
+        }
+    }
+}
---
+++ Ruben_Booking.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Ruben_Booking.API.Services;
+using Ruben_Booking.API.Services.Interfaces;
+
+namespace Ruben_Booking.API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, EmployeeService>();
+            services.AddScoped<ConsultantService>();
+            services.AddScoped<ILoginService, LoginService>();
+            services.AddScoped<IRoomService, RoomService>();
+
+            services.AddCors(options =>
+                options.AddPolicy("AllowAll", policy =>
+                    policy.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                )
+            );
+
+            return services;
+        }
+        public static IServiceCollection AddDefaultServices(this IServiceCollection services) 
+        {
+            services.AddAuthorization();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddLogging();
+
+            return services;
+        }
+        
+    }
+}
