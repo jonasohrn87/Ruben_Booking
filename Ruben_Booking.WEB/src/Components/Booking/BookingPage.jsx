3e60d4168d1daf77258a99d60cc3a71adf666fd3
Rasmus Broman <bromanr@outlook.com>
2024-12-06 11:57:23 +0100

    Added service and funktion to fetch rooms. Added design to show room configuration.

--- Ruben_Booking.WEB/src/Components/Booking/BookingPage.jsx
+++ Ruben_Booking.WEB/src/Components/Booking/BookingPage.jsx
@@ -1,12 +1,38 @@
-import React from "react";
-import LogoutButton from "../Logout/LogoutButton";
+import React, { useState, useEffect } from "react";
+import BookableRoom from "../Room/BookableRoom";
+import { getRoomService } from "../../Services/apiService";
 
 const BookingPage = () => {
-    return (
-        <div className="bookingPage-container">
-            <h3>Boka rum här</h3>
-        </div>
-    );
-}
+  const [bookableRoomInfo, setBookableRoomInfo] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    const fetchRooms = async () => {
+      try {
+        const rooms = await getRoomService.getAllRooms();
+        setBookableRoomInfo(rooms);
+      } catch (errorFetchingRooms) {
+        setErrorMessage("Kunde inte hämta rum, försök igen senare");
+        console.error(errorFetchingRooms);
+      }
+    };
+    fetchRooms();
+  }, []);
+
+  if (errorMessage) {
+    return <div>{errorMessage}</div>;
+  }
+
+  return (
+    <div className="bookingPage-container">
+      <h3>Boka rum här</h3>
+      <div className="bookingPage-rooms">
+        {bookableRoomInfo.map((room) => (
+          <BookableRoom key={room.id} {...room} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default BookingPage;
--- Ruben_Booking.WEB/src/Components/Room/BookableRoom.jsx
+++ Ruben_Booking.WEB/src/Components/Room/BookableRoom.jsx
@@ -1,20 +1,40 @@
-import React, { useState } from "react";
-
-const BookableRoom = ({ id, MaxSeats, Name, Description, IsInService, HasProjector, HasWhiteBoard}) => {
-
-    return (
-        <div className="bookableRoom-container">
-            <div>
-
-            <h4>{Name}</h4>
-            <p>{Description}</p>
-            <p>{MaxSeats} platser</p>
-            <p>{HasProjector ? "Har projektor" : "Har ej projektor"}</p>
-            </div>
-            <p>{HasWhiteBoard ? "Har whiteboard" : "Har ej whiteboard"}</p>
-            <h3>{IsInService ? "" : "Ur funktion"}</h3>
+import React from "react";
+import PropTypes from "prop-types";
+const BookableRoom = ({
+  id,
+  maxSeats,
+  name,
+  description,
+  isInService,
+  hasProjector,
+  hasWhiteBoard,
+}) => {
+  return (
+    <div>
+      <p className="bookableRoom-roomNumber">Rum {id}</p>
+      <div className="bookableRoom-container">
+        <div>
+          <h4>{name}</h4>
+          <p>{description}</p>
+          <p>{maxSeats} platser</p>
+          <p>{hasProjector ? "Har projektor" : "Har ej projektor"}</p>
+          <p>{hasWhiteBoard ? "Har whiteboard" : "Har ej whiteboard"}</p>
+          <h3>{isInService ? "" : "Ur funktion"}</h3>
         </div>
-    );
-}
+        {isInService ? <button>Boka rum</button> : <p>Rummet är ur funktion</p>}
+      </div>
+    </div>
+  );
+};
+
+BookableRoom.propTypes = {
+  id: PropTypes.number.isRequired,
+  maxSeats: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  isInService: PropTypes.bool.isRequired,
+  hasProjector: PropTypes.bool.isRequired,
+  hasWhiteBoard: PropTypes.bool.isRequired,
+};
 
 export default BookableRoom;
