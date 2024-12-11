37f7b0431c99ff8eadcb1f9eadacb4555d56fea7
Rasmus Broman <bromanr@outlook.com>
2024-12-11 10:00:06 +0100

    Added more seed to database, booking test and copy of database for tests.

---
+++ Ruben_Booking.API.Tests/Database/TestCopyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ruben_Booking.API.Database.Models;
+using Microsoft.EntityFrameworkCore;
+using Ruben_Booking.API.Database;
+
+namespace Ruben_Booking.API.Tests.Database
+{
+    public class TestCopyDbContext : RubenContext
+    {
+        public TestCopyDbContext(DbContextOptions<RubenContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Ruben_Booking.API/Migrations/20241210234708_AddedBookingData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Ruben_Booking.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedBookingData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Bookings",
+                columns: new[] { "Id", "DateFrom", "DateTo", "Participants", "PersonId", "RoomId" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 12, 20, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2024, 12, 20, 23, 59, 59, 0, DateTimeKind.Local), "[\"Hasse Jansson\"]", 1, 1 },
+                    { 2, new DateTime(2024, 12, 22, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2024, 12, 24, 23, 59, 59, 0, DateTimeKind.Local), "[]", 2, 2 },
+                    { 3, new DateTime(2024, 12, 19, 12, 0, 0, 0, DateTimeKind.Local), new DateTime(2024, 12, 19, 16, 0, 0, 0, DateTimeKind.Local), "[]", 3, 3 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Bookings",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Bookings",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Bookings",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
